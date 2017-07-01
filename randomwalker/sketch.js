68d35a49641eff7c7b019a432d0006478b0f857c
joeydash <joydassudiptta@gmail.com>
2017-07-01 11:51:48 +0530

    random walker done

---
+++ randomwalker/sketch.js
@@ -0,0 +1,34 @@
+var walker;
+function setup() {
+  createCanvas(600,600);
+  background(200);
+  walker= new walker();
+}
+function draw(){
+	walker.render();
+	walker.step();
+	
+}
+function walker(){
+	this.x=width/2;
+	this.y=height/2;
+	this.render = function(){
+		stroke(0);
+		fill(0);
+		ellipse(this.x,this.y,3,3);
+	}
+	this.step  = function(){
+		var x= floor(random(4));
+		if (x==0) {
+			this.x++;
+		}if (x==1) {
+			this.x--;
+		}if (x==2) {
+			this.y++;
+		}if (x==3){
+			this.y--;
+		}
+		constrain(this.x,0,width);
+		constrain(this.y,0,height);
+	}
+}
