cbc0bdde234c8da519259687b05d2f63176bc686
joeydash <joydassudiptta@gmail.com>
2017-07-01 18:56:17 +0530

    done

--- vectors/sketch.js
+++ vectors/sketch.js
@@ -1,32 +1,39 @@
-var ball;
+var balls=[];
 function setup() {
-  createCanvas(600,600,[WEBGL]);
-  ball = new ball;
+  createCanvas(windowWidth,windowHeight,[WEBGL]);
+	for (var i = 0; i < 500; i++) {
+		balls[i]= new ball;
+	}
 }
 function draw(){
-	background(200);
-	ball.render();
-}
-function mouseClicked(){
-	ball.setlocation();
+	background('#365698');
+	fill('#EA783B');
+	for (var i = 0; i < balls.length; i++) {
+		balls[i].render();
+	}
+	translate(width/2,height/2);
+	textSize(80);
+	fill(255);
+	stroke(0);
+	textFont("Verdana");
+	textAlign(CENTER);
+	text("JOEYDASH",0,0);
+
 }
 function ball(){
-	this.location=createVector(width/2,height/2);
-	this.velocity=createVector(-1,-1);
+	this.location=createVector(random(0,width),random(0,height));
+	this.velocity=createVector(random([1,-1]),random([1,-1]));
 	this.render = function () {
 		this.location.add(this.velocity);
 		ellipse(this.location.x,this.location.y,10,10);
-		if (this.location.x>590) {
+		if (this.location.x>width-10) {
 			this.velocity.x=-this.velocity.x;
 		}if (this.location.x<10) {
 			this.velocity.x=-this.velocity.x;
-		}if (this.location.y>590) {
+		}if (this.location.y>height-10) {
 			this.velocity.y=-this.velocity.y;
 		}if (this.location.y<10) {
 			this.velocity.y=-this.velocity.y;
-		}
-	}
-	this.setlocation = function() {
-		this.location=createVector(mouseX,mouseY);		
+		}if () {}
 	}
 }
